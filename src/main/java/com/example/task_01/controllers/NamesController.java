e8e1c5dc84c0507780275a2bca7c563cd3df84fa
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-22 15:39:10 +0000

    Implemented @RequestParam for timeOfDay in greet() method in controller

--- src/main/java/com/example/task_01/controllers/NamesController.java
+++ src/main/java/com/example/task_01/controllers/NamesController.java
@@ -2,19 +2,17 @@ package com.example.task_01.controllers;
 
 
 import com.example.task_01.models.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/greeting")
 public class NamesController {
 
     @GetMapping
-    public String greet(){
-        Greeting greeting = new Greeting("Colin","afternoon");
+    @ResponseBody
+    public String greet(@RequestParam (required = false) String timeOfDay){
+        Greeting greeting = new Greeting("Colin",timeOfDay);
         return String.format("Good %s %s!", greeting.getTimeOfDay(), greeting.getName());
     }
-
 
 }
