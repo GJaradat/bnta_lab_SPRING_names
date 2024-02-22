8b24c3ca806b01b3027a76054de5a57c5c7fd53a
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-22 15:50:37 +0000

    Added Celebration at /greeting/summer. Made timeOfDay parameter request optional, with default value to morning

--- src/main/java/com/example/task_01/controllers/NamesController.java
+++ src/main/java/com/example/task_01/controllers/NamesController.java
@@ -1,6 +1,7 @@
 package com.example.task_01.controllers;
 
 
+import com.example.task_01.models.Celebration;
 import com.example.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,16 @@ public class NamesController {
     @ResponseBody
     public String greet(@RequestParam (required = false) String timeOfDay){
         Greeting greeting = new Greeting("Colin",timeOfDay);
+        if (greeting.getTimeOfDay() == null){
+            greeting.setTimeOfDay("morning");
+        }
         return String.format("Good %s %s!", greeting.getTimeOfDay(), greeting.getName());
+    }
+
+    @GetMapping(value = "/summer")
+    public String celebrate(){
+        Celebration yay = new Celebration("Happy summer solstice!");
+        return yay.getMsg();
     }
 
 }
---
+++ src/main/java/com/example/task_01/models/Celebration.java
@@ -0,0 +1,20 @@
+package com.example.task_01.models;
+
+public class Celebration {
+
+    private String msg;
+
+    public Celebration(){}
+
+    public Celebration(String msg){
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
