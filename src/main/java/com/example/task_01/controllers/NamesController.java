dc02a721420bcfc2f329bc8e63dfa5f8e2c56a30
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-22 15:24:39 +0000

    Added POJO to NamesController

---
+++ src/main/java/com/example/task_01/controllers/NamesController.java
@@ -0,0 +1,20 @@
+package com.example.task_01.controllers;
+
+
+import com.example.task_01.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greeting")
+public class NamesController {
+
+    @GetMapping
+    public String greet(){
+        Greeting greeting = new Greeting("Colin","afternoon");
+        return String.format("Good %s %s!", greeting.getTimeOfDay(), greeting.getName());
+    }
+
+
+}
